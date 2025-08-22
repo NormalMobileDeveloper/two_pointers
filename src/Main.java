fdedaadf13fbca703e76a59b35e5be93f9cbe9a0
Nurmuhammad Abdirashidov <nurmuhammadadeveloper@gmail.com>
2025-08-22 20:18:39 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+       var main = new Main();
+       var arr = new int[] {1,1,1,2,2,3,3,3,3};
+       main.twoPointers(arr);
+    }
+
+    public void twoPointers(int[] arr) {
+        var left = 0;
+        var right = left + 1;
+
+        while (right < arr.length) {
+            if (arr[left] != arr[right]) {
+                arr[++left] = arr[right];
+            }
+            right++;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
